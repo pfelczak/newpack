0048c63b1c7cec4b2f15614881a0836303099fe7
PiotrF <41piofel@gmail.com>
2020-11-03 23:09:41 +0100

    poprawka mojej funkcji

--- R/fun_pack.R
+++ R/fun_pack.R
@@ -1,8 +1,12 @@
 fun_pack <- function(a = 10, b = 20, d = 2){
-  out <- sqrt(a*b*d/2 + 100)
+
+  out <- sqrt(a*b*d/2 + 100 +1)
+
   out <- rnorm(n = round(out,0)) %>%
     as.data.frame()
+
   out <- out %>%
     summarise(mean(.,na.rm = T))
+
   return(out)
 }
