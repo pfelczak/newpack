2eab75ec8c8412acdc6359520b3cdb8fa11028dd
PiotrF <41piofel@gmail.com>
2020-11-04 10:25:21 +0100

    funkcja emisji

--- R/fun_pack.R
+++ R/fun_pack.R
@@ -1,12 +1,30 @@
-fun_pack <- function(a = 10, b = 20, d = 2){
+fun_pack <- function(dane = input,
+                     kategoria = "Passenger Cars",
+                     #paliwo = "Petrol",
+                     #segment = "Mini",
+                     euro = "Euro 5",
+                     #technologia = "GDI",
+                     mode = "",
+                     substancja = c("EC", "CO")) {
 
-  out <- sqrt(a*b*d/2 + 100+1)
+  # Zabezpieczenia argumentów funkcji i danych wejscioweych.
 
-  out <- stats::rnorm(n = base::round(out,0))
-  out <- base::as.data.frame(out)
+  out <- wskazniki %>%
+    filter(Category %in% kategoria) %>%
+    filter(Fuel %in% paliwo) %>%
+    filter(Euro.Standard %in% euro) %>%
+    filter(Technology %in% technologia) %>%
+    filter(Pollutant %in% substancja) %>%
+    filter(Mode %in% mode)
 
-  out <- dplyr::summarise(out,
-                         mean = base::mean(out,na.rm = T))
+  out <- inner_join(x = out, y = input, by = "Segment")
+
+  out <- out %>%
+    mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
+                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction))
+    ) %>%
+    select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Nat, Emisja)
 
   return(out)
+
 }
